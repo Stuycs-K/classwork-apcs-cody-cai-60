af8f635e14acba0a0b86685d215427898606b7f0
Cody Cai <ccai60@stuy.edu>
2024-09-27 15:18:43 -0400

    Fixed test cases for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -10,28 +10,30 @@ public class MyArrays {
     return result + "]";
   }
   public static int[] returnCopy (int[] ary) {
-    return 0;
+    int[] result = {};
+    return result;
   }
   public static int[] concatArray (int[] ary1, int[] ary2) {
-    return 0;
+    int[] result = {};
+    return result;
   }
   public static void main(String[] args) {
     //testing returnCopy
     int[] ary = {1, 9, 7};
     int[] ary2 = returnCopy(ary);
-    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Same? " + ary != ary2);
-    ary = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    ary2 = returnCopy(ary);
-    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Same? " + ary != ary2);
-    ary = {1, 0, 0, 0, 0, 9};
-    ary2 = returnCopy(ary);
-    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Same? " + ary != ary2);
-    ary = {};
-    ary2 = returnCopy(ary);
-    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Same? " + ary != ary2);
-    ary = {0};
-    ary2 = returnCopy(ary);
-    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Same? " + ary != ary2);
+    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Different Copy? " + !(ary == ary2) + "Same Elements? " + arrayToString(ary).equals(arrayToString(ary2)));
+    int[] ary3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[] ary4 = returnCopy(ary);
+    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Different Copy? " + !(ary3 == ary4) + "Same Elements? " + arrayToString(ary3).equals(arrayToString(ary4)));
+    int[] ary5 = {1, 0, 0, 0, 0, 9};
+    int[] ary6 = returnCopy(ary);
+    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Different Copy? " + !(ary5 == ary6) + "Same Elements? " + arrayToString(ary5).equals(arrayToString(ary6)));
+    int[] ary7 = {};
+    int[] ary8 = returnCopy(ary);
+    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Different Copy? " + !(ary7 == ary8) + "Same Elements? " + arrayToString(ary7).equals(arrayToString(ary8)));
+    int[] ary9 = {0};
+    int[] ary10 = returnCopy(ary);
+    System.out.println("Array 1: " + arrayToString(ary) + " Array 2: " + arrayToString(ary2) + "Different Copy? " + !(ary9 == ary10) + "Same Elements? " + arrayToString(ary9).equals(arrayToString(ary10)));
 
     //testing concatArray
 
