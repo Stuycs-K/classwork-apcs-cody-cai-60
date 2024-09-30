0832bb7fbb73adfbb07e5aedaf7882613ab3f023
Cody Cai <ccai60@stuy.edu>
2024-09-30 12:32:16 -0400

    Added arr2DSum and test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -40,13 +40,18 @@ public class ArrayMethods {
         answer += "], [";
       }
     }
-    return answer + "]]""
+    return answer + "]]";
   }
 
   /*Return the sum of all of the values in the 2D array */
   public static int arr2DSum(int[][]nums){
-    //use a nested loop to solve this
-    return null;//place holder return value so it compiles.
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        sum += nums[i][j];
+      }
+    }
+    return sum;//place holder return value so it compiles.
   }
 
   /**Rotate an array by returning a new array with the rows and columns swapped.
@@ -60,17 +65,30 @@ public class ArrayMethods {
   public static void main (String[] args) {
 
     //testing arrToString(int[][]ary)
-    int[][] ary1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    System.out.println("Expected: {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}} | Return: " + arrToString(ary1).equals("{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}"));
-    int[][] ary2 = {{1, 2}, {4, 5}, {7, 8}};
-    System.out.println("Expected: {{1, 2}, {4, 5}, {7, 8}} | Return: " + arrToString(ary2).equals("{{1, 2}, {4, 5}, {7, 8}}"));
-    int[][] ary3 = {{}, {}, {}};
-    System.out.println("Expected: {{}, {}, {}} | Return: " + arrToString(ary3).equals("{{}, {}, {}}"));
-    int[][] ary4 = {{1, 2, 3, 4}, {5, 6}, {7, 8, 9, 10, 11}};
-    System.out.println("Expected: {{1, 2, 3, 4}, {5, 6}, {7, 8, 9, 10, 11}} | Return: " + arrToString(ary4).equals("{{1, 2, 3, 4}, {5, 6}, {7, 8, 9, 10, 11}}"));
-    int[][] ary5 = {{1, 2, 3}, {}, {7, 8, 9, 10}};
-    System.out.println("Expected: {{1, 2, 3}, {}, {7, 8, 9, 10}} | Return: " + arrToString(ary5).equals("{{1, 2, 3}, {}, {7, 8, 9, 10}}"));
+    int[][] ary1a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println("Expected: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] | Return: " + arrToString(ary1a) +  " | Match? " + arrToString(ary1a).equals("[[1, 2, 3], [4, 5, 6], [7, 8, 9]]"));
+    int[][] ary2a = {{1, 2}, {4, 5}, {7, 8}};
+    System.out.println("Expected: [[1, 2], [4, 5], [7, 8]] | Return: " + arrToString(ary2a) +  " | Match? " + arrToString(ary2a).equals("[[1, 2], [4, 5], [7, 8]]"));
+    int[][] ary3a = {{}, {}, {}};
+    System.out.println("Expected: [[], [], []] | Return: " + arrToString(ary3a) +  " | Match? " + arrToString(ary3a).equals("[[], [], []]"));
+    int[][] ary4a = {{1, 2, 3, 4}, {5, 6}, {7, 8, 9, 10, 11}};
+    System.out.println("Expected: [[1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]] | Return: " + arrToString(ary4a) +  " | Match? " + arrToString(ary4a).equals("[[1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]]"));
+    int[][] ary5a = {{1, 2, 3}, {}, {7, 8, 9, 10}};
+    System.out.println("Expected: [[1, 2, 3], [], [7, 8, 9, 10]] | Return: " + arrToString(ary5a) +  " | Match? " + arrToString(ary5a).equals("[[1, 2, 3], [], [7, 8, 9, 10]]"));
 
+    //testing arr2DSum
+    int[][] ary1b = {{1, 2, 3, 4}, {5, 6, 7, 8,}, {9, 10, 11, 12}};
+    System.out.println("Expected: 78 | Return: " + arr2DSum(ary1b) + " | Match? " + (arr2DSum(ary1b) == 78));
+    int[][] ary2b = {{1, 2, 3}, {5, 6}, {9, 10, 11, 12}};
+    System.out.println("Expected: 59 | Return: " + arr2DSum(ary2b) + " | Match? " + (arr2DSum(ary2b) == 59));
+    int[][] ary3b = {{1}, {5, 8}, {}};
+    System.out.println("Expected: 14 | Return: " + arr2DSum(ary3b) + " | Match? " + (arr2DSum(ary3b) == 14));
+    int[][] ary4b = {{}, {}, {0}};
+    System.out.println("Expected: 0 | Return: " + arr2DSum(ary4b) + " | Match? " + (arr2DSum(ary4b) == 0));
+    int[][] ary5b = {{0, 0, 0, 0}, {0, 0}, {0, 0, 0}};
+    System.out.println("Expected: 0 | Return: " + arr2DSum(ary5b) + " | Match? " + (arr2DSum(ary5b) == 0));
+    int[][] ary6b = {{}, {}, {}};
+    System.out.println("Expected: 0 | Return: " + arr2DSum(ary6b) + " | Match? " + (arr2DSum(ary6b) == 0));
 
   }
 
