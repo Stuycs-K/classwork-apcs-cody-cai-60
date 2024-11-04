4ee05e1934461a42d4499e8d05f976a3ea103daa
Cody Cai <ccai60@stuy.edu>
2024-11-04 11:31:26 -0500

    added countTrianglesA and it compiled

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriangleTester {
 
   public static boolean checkTriangle(int a, int b, int c) {
@@ -9,11 +13,27 @@ public class TriangleTester {
   }
 
   public static int countTrianglesA(String filename) {
-    return -1;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int count = 0;
+      while (input.hasNextLine() && input.hasNextInt()) {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if (checkTriangle(a, b , c)) {
+          count++;
+        }
+      }
+      input.close();
+      return count;
+    } catch (FileNotFoundException e) {
+      return -1;
+    }
   }
 
   public static void main(String[] args) {
-
+    System.out.println(countTrianglesA("inputTri.txt"));
   }
 
 }
