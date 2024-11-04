861a1ff7010f973a9bb1ee415908b424ca5c2852
Cody Cai <ccai60@stuy.edu>
2024-11-04 11:16:02 -0500

    added helper function checkTriangle (a, b, c)

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,5 +1,13 @@
 public class TriangleTester {
 
+  public static boolean checkTriangle(int a, int b, int c) {
+    if ((a + b > c) || (a + c > b) || (b + c > a)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public static int countTrianglesA(String filename) {
     return -1;
   }
