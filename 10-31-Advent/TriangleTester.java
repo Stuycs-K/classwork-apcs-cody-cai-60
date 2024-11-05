036b1b1adcc135708125b8b3048655f3665bd6d9
Cody Cai <ccai60@stuy.edu>
2024-11-04 21:58:39 -0500

    working part b

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -40,8 +40,30 @@ public class TriangleTester {
       File file = new File(filename);
       Scanner input = new Scanner(file);
       int count = 0;
-      while (input.hasNextLine() && input.hasNextInt()) {
-        
+      String tri1 = "";
+      String tri2 = "";
+      String tri3 = "";
+      while (input.hasNextInt() && input.hasNextLine()) {
+        for (int i = 0; i < 3; i++) {
+          tri1 += input.nextInt() + " ";
+          tri2 += input.nextInt() + " ";
+          tri3 += input.nextInt() + " ";
+        }
+        String[] tri1Arr = tri1.split(" ");
+        String[] tri2Arr = tri2.split(" ");
+        String[] tri3Arr = tri3.split(" ");
+        if (checkTriangle(Integer.parseInt(tri1Arr[0]), Integer.parseInt(tri1Arr[1]), Integer.parseInt(tri1Arr[2]))) {
+          count++;
+        }
+        if (checkTriangle(Integer.parseInt(tri2Arr[0]), Integer.parseInt(tri2Arr[1]), Integer.parseInt(tri2Arr[2]))) {
+          count++;
+        }
+        if (checkTriangle(Integer.parseInt(tri3Arr[0]), Integer.parseInt(tri3Arr[1]), Integer.parseInt(tri3Arr[2]))) {
+          count++;
+        }
+        tri1 = "";
+        tri2 = "";
+        tri3 = "";
       }
       return count;
 
@@ -50,15 +72,12 @@ public class TriangleTester {
     }
   }
 
-
-
-
   public static void main(String[] args) {
     //Testing part a
-    //System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesA("inputTri.txt")); //correct answer 52
 
     //Testing part break;
-    System.out.println(countTrianglesB("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt")); //correct answer 95
   }
 
 }
