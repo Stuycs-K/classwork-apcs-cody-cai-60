92debc6282c4c39ed7b5a29fa8e0f96a4c113208
Cody Cai <ccai60@stuy.edu>
2024-11-07 21:01:52 -0500

    finished testing all new objects

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -3,6 +3,7 @@
 // This is because although bird is an Animal, it has access to the Bird methods
 //Explanation for Part 10:
 // Bird is-a Animal, but Animal is not a Bird
+// A Bird has all the methods of Animal, but an Animal does not have all the methods of Bird
 
 public class Driver{
 
@@ -15,9 +16,12 @@ public class Driver{
 
     //testing step 9
     Animal a1 = new Animal("Bobby", 12, "boop");
+    a1.speak();
     Bird b1 = new Bird("Billy", 14, "beep", 5.7, "red");
+    b1.speak();
     //Bird b2 = new Animal("Belly", 13, "blop");    ERROR
     Animal a2 = new Bird("Bully", 15, "blip", 2, "yellow");
+    a2.speak();
   }
 
 }
