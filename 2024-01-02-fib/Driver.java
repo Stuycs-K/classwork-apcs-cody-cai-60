6e7b6ac5da9fc991b75bac22e0a14ccdcaf907e6
Cody Cai <ccai60@stuy.edu>
2025-01-02 11:46:22 -0500

    finished fib

---
+++ 2024-01-02-fib/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+  public static void main(String[] args) {
+
+    System.out.println(Fib.fib(Integer.parseInt(args[0])));
+    // largest n: 46
+    // time: 6.99 s
+    // number: 1836311903
+
+  }
+
+}
---
+++ 2025-01-02-fib/Fib.java
@@ -0,0 +1,11 @@
+public class Fib {
+
+  public static int fib(int n) {
+    if (n < 2) {
+      return n;
+    } else {
+      return fib(n-1) + fib(n-2);
+    }
+  }
+
+}
